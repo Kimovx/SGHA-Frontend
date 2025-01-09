84919df0989a7d9da13ffdd8015674a01bef4a31
Karim H. Essa <dj.mr.jony@gmail.com>
2025-01-10 01:12:21 +0200

    Added Login Page

--- src/app/Components/system-login/system-login.component.ts
+++ src/app/Components/system-login/system-login.component.ts
@@ -1,42 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoginRequest } from '../../Model/LoginRequesr';
 import { firstValueFrom } from 'rxjs';
 import { AuthService } from '../../../Services/auth.service';
+import {MatRippleModule} from '@angular/material/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-system-login',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatRippleModule
+  ],
+  animations: [
+    trigger('heightAnimation', [
+      transition(':enter', [
+        style({ height: '0px', opacity: 0 }),
+        animate('250ms ease-out', style({ height: '*', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('150ms ease-out', style({ height: '0px', opacity: 0 }))
+      ])
+    ]),
+    trigger('heightWidthAnimation', [
+      transition(':enter', [
+        style({ height: '0px', width: '0px', opacity: 0 }),
+        animate('250ms ease-out', style({ height: '*', width: '*', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('150ms ease-out', style({ height: '0px', width: '0px', opacity: 0 }))
+      ])
+    ])
   ],
   templateUrl: './system-login.component.html',
   styleUrl: './system-login.component.css'
 })
 export class SystemLoginComponent implements OnInit {
   loginRequest: LoginRequest;
-  
+
+  // Boolean Variables
+  isInvalid: boolean
+  isLoading: boolean
+
   constructor(
     private _formBuilder: FormBuilder,
     private authService: AuthService
   ){
     this.loginRequest = {} as LoginRequest;
+
+    // Boolean Variables
+    this.isInvalid = false;
+    this.isLoading = false;
   }
 
   ngOnInit(): void {}
 
+  @HostListener('input', ['$event'])
+  onInput(){
+    this.isInvalid = false;
+  }
+
   loginFormGroup = this._formBuilder.group({
-    emailAddress: ['', Validators.required],
-    accountPassword: ['', Validators.required]
+    emailAddress: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]],
+    accountPassword: ['', [Validators.required, Validators.minLength(8)]]
   });
 
   async onSubmit(event: Event){
     event.preventDefault();
+    this.isLoading = true;
 
     this.loginRequest.emailAddress = this.loginFormGroup.get('emailAddress')?.value!;
     this.loginRequest.accountPassword = this.loginFormGroup.get('accountPassword')?.value!;
 
-    console.log(await firstValueFrom(this.authService.loginRequest(this.loginRequest)))
+    const res = await firstValueFrom(this.authService.loginRequest(this.loginRequest)).then(()=> this.isLoading = false).catch(()=> this.isInvalid = true)
+    console.log(res);
   }
 
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideClientHydration(),
+    provideAnimationsAsync(),
   ]
 };
