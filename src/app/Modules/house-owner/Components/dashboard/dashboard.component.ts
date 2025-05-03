b2d6742d619c047559cccfefe4002a3eb529c0f4
Karim H. Essa <dj.mr.jony@gmail.com>
2025-05-03 21:36:27 +0300

    Update Dashboard

--- src/app/Modules/house-owner/Components/dashboard/dashboard.component.ts
+++ src/app/Modules/house-owner/Components/dashboard/dashboard.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { SideNavComponent } from '../side-nav/side-nav.component';
+import { CommonModule } from '@angular/common';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    SideNavComponent,
+    MatSlideToggleModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
