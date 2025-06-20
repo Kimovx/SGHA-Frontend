540f34fc5945dc04f2c845d0cd34fa8d7f6c98b2
Karim H. Essa <113004991+Kimovx@users.noreply.github.com>
2025-06-20 15:02:27 +0300

    feat: add notification permission request in ngOnInit for better user experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,8 +26,31 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.signalRService.startConnection();
+    this.askNotificationPermission();
   }
 
+  askNotificationPermission() {
+  if ('Notification' in window) {
+    if (Notification.permission === 'default') {
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          console.log('🔔 Notification permission granted!');
+        } else if (permission === 'denied') {
+          console.log('❌ Notification permission denied.');
+        }
+      });
+    } else if (Notification.permission === 'granted') {
+      console.log('✅ Notification permission already granted.');
+    } else {
+      console.log('❌ Notification already denied.');
+    }
+  } else {
+    console.warn('⚠️ Browser does not support notifications.');
+  }
+}
+
+
+  // This method is used to prepare the route for animations
   prepareRoute(outlet: RouterOutlet | null) {
     if (!outlet || !outlet.isActivated) {
       return 'none';
